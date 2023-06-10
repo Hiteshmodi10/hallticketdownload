e533677d7ea7982ffad5a23aaebd4563d16df736
Hiteshmodi10 <124600876+Hiteshmodi10@users.noreply.github.com>
2023-06-10 09:24:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function submitInput(event){
      personName = formData.get("personName"),
      oldEnrollementNo = formData.get("oldEnrollement")
 
-    url = `http://127.0.0.1:5500//result.html?seatNo=${encodeURIComponent(seatNo)}&enrollementNo=${encodeURIComponent(enrollementNo)}&personName=${encodeURIComponent(personName)}&oldEnrollementNo=${oldEnrollementNo}`;
+    url = `https://sensational-caramel-7f7e8a.netlify.app?seatNo=${encodeURIComponent(seatNo)}&enrollementNo=${encodeURIComponent(enrollementNo)}&personName=${encodeURIComponent(personName)}&oldEnrollementNo=${oldEnrollementNo}`;
     
         document.location.href = url;
 
