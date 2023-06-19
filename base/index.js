b6671598e9e2519dfd35a216d6cf261a742939da
Hiteshmodi10 <124600876+Hiteshmodi10@users.noreply.github.com>
2023-06-19 13:52:03 +0530

    Update index.js

--- base/index.js
+++ base/index.js
@@ -1,29 +1,30 @@
 class App {
   async init() {
-    await this.loadComponents();
+    await this.loadInput();
     await this.loadUI();
   }
-  async loadComponents() {
-    await new Import("/components/input.js").load();
+
+  async loadInput() {
+    await new Import("./components/input.js").load();
   }
+
   async loadUI() {
-    const components = document.querySelectorAll("[data-component]");
-    components.forEach((el) => {
-      if (el.dataset.component === "input") {
-        const input = new Input({
-            placeholder: el.dataset.configPlaceholder,
-            name: el?.dataset?.configName,
-            autoComplete: el?.dataset?.configAutoComplete,
-            id: el.dataset.configId,
-        }).getElement();
-        el.appendChild(input);
-      }
+    const component = document.querySelectorAll("[data-component]");
+    component.forEach((el) => {
+      const input = new Input({
+        placeholder: el.dataset.configPlaceholder,
+        name: el.dataset.configName,
+        id: el.dataset.configId,
+        autoComplete: el.dataset.configAutoComplete,
+        maxlength: el.dataset.configMaxlength,
+      }).getElement();
+      el.appendChild(input);
     });
   }
 }
 
 const app = new App();
 
-(async ()=> {
-    await app.init();
+(async () => {
+  await app.init();
 })();
